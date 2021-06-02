3ad68ae9a6d7450e1b07562477fed67467b1c924
Khanturov Daniil <soloexploder@gmail.com>
2021-06-02 15:56:11 +0300

    Initial Commit

---
+++ TrafficlightUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficlightUI
+//
+//  Created by Даниил Хантуров on 02.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficlightUI/TrafficlightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficlightUIApp.swift
+//  TrafficlightUI
+//
+//  Created by Даниил Хантуров on 02.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficlightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
